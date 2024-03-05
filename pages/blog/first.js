c29cb6fb656dedf370fc3cd31ed29d068413dbc0
Truong Duc Khai <khai.truong-duc@banvien.com.vn>
2024-03-06 01:01:21 +0700

    research router in nextjs

---
+++ pages/blog/first.js
@@ -0,0 +1,5 @@
+function FirstBlog(props) {
+  return <h1>First blog page</h1>;
+}
+
+export default FirstBlog;
---
+++ pages/blog/second.js
@@ -0,0 +1,5 @@
+function SecondBlog(props) {
+  return <h1>Second blog page</h1>;
+}
+
+export default SecondBlog;
