fde498c96ed21a66415d35741c8d928c325252b1
Truong Duc Khai <khai.truong-duc@banvien.com.vn>
2024-03-06 22:50:07 +0700

    Research next.js routing and 404 not found page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 function Home(props) {
-  return <h1>Home page</h1>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/product");
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Product</Link>
+      <button onClick={handleClick}>Place order</button>
+    </div>
+  );
 }
 
 export default Home;
--- pages/product/[productId]/review/[reviewId].js
+++ pages/product/[productId]/review/[reviewId].js
@@ -4,6 +4,8 @@ function Review(props) {
   const router = useRouter();
   const { productId, reviewId } = router.query;
 
+  console.log(router.query);
+
   return (
     <h1>
       Review {reviewId} for product {productId}
