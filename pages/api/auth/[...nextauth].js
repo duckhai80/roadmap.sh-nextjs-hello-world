0f330b8f4b587124f4b208898937d601fe40a719
Truong Duc Khai <duckhai80@gmail.com>
2024-04-02 01:32:03 +0700

    Connecting to database and authentication summary

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth/next";
 import GitHubProviders from "next-auth/providers/github";
+// import clientPromise from "../mongodb";
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,23 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  // session: {
+  //   jwt: true,
+  // },
+  // jwt: {
+  //   secret: "randomsecretkey",
+  // },
+  // adapter: MongoDBAdapter(clientPromise),
+  // callbacks: {
+  //   async jwt(token, user) {
+  //     if (user && token) {
+  //       token.id = user.id;
+  //     }
+  //     return token;
+  //   },
+  //   async session(session, token) {
+  //     if (session && token) session.user.id = token.id;
+  //     return session;
+  //   },
+  // },
 });
--- pages/users.js
+++ pages/users.js
@@ -1,6 +1,6 @@
 // import User from "../components/User";
 
-import User from "@/components/User";
+import User from "@/components/user";
 
 function UserList({ users }) {
   return (
